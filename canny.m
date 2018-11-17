4132676c1564b2b1bbb34e5f074e3f9f509c2972
yannic <Yannic.wlns@gmail.com>
2018-11-17 23:18:36 +0100

    fudge it

--- canny.m
+++ canny.m
@@ -1,3 +1,14 @@
-function return_value = canny(parameter)
+function return_value = canny(gameNr, roundNr)
     return_value = 1;
+    
+    f = filename(gameNr, roundNr);
+    
+    
+    img = imread(['assets/geometric_transformation/G' f]);
+    
+    im = imdilate(img,strel('disk',2));
+    BW = imerode(im,strel('disk',2));
+    BW = edge(BW,'Canny');
+
+    imshow(BW);
 end
