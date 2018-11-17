e5309a8bfc11a84686e6813abe12fa0804cd7e89
yannic <Yannic.wlns@gmail.com>
2018-11-17 23:01:55 +0100

    fixed geometric trasnformation

--- geometric_transformation.m
+++ geometric_transformation.m
@@ -1,15 +1,15 @@
 function geometric_transformation(gameNr, roundNr, corners)
     f = filename(gameNr, roundNr);
-    
     img = imread(['assets/threshold/T' f]);
 
-    %[c r p] = impixel;
     base = [0 10; 10 10; 10 0; 0 0];
     tf = fitgeotrans(corners,base*50,'projective');
-
+    
     [xf1, xf1_ref] = imwarp(img,tf);
-    imshow(xf1)
-    xf1_ref
-    xf1 = imrotate(xf1, 180);
-    imwrite(xf1, fullfile('assets/geometric_transformation/', ['registered_', f]));
+    [xdataT,ydataT] = transformPointsForward(tf,corners(1:4),corners(5:8));
+    [xdataI, ydataI] = worldToIntrinsic(xf1_ref,xdataT,ydataT);
+    
+    croped = imcrop(xf1,[min(xdataI) min(ydataI) 499 499]);
+    croped = imrotate(croped, 180);
+    imwrite(croped, fullfile('assets/geometric_transformation/', ['G', f]));
 end
