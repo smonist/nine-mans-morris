dc0dd7a96ebfb33c70272885f78950224bfc09cb
yannic <Yannic.wlns@gmail.com>
2018-12-28 14:27:38 +0100

    hey simon <3

--- canny.m
+++ canny.m
@@ -1,10 +1,10 @@
 function return_value = canny(gameNr, roundNr)
-    return_value = 1;
     f = filename(gameNr, roundNr);
-    
     img = imread(['assets/geometric_transformation/G' f]);
-    img = imbinarize(img, 0.9);
-    img = imdilate(img,strel('disk',2));
-    img = edge(img,'Canny');
-    imwrite(img, fullfile('assets/Canny/', ['E', f]));
+    
+    img = imbinarize(img);
+    
+    imshow(img);
+    
+    return_value = 1;
 end
