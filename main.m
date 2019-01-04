755f8c13cf7ad44ebeeb0f1c8f79eeb4d2903bc8
yannic <Yannic.wlns@gmail.com>
2019-01-04 17:33:06 +0100

    matlab bullshit

--- main.m
+++ main.m
@@ -2,10 +2,10 @@
 game = 3;
 
 %threshold: float <= 1 && >= 0
-threshold = 0.5;
+custom_threshold = 0.5;
 
 for i = 2 : length(dir(strcat('assets/original/', int2str(game), '_*.jpg')))
-    corners = threshold(game,i,threshold);
+    corners = threshold(game,i,custom_threshold);
     geometric_transformation(game, i, corners);
     canny(game, i);
     hough(game, i); 
