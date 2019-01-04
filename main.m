29652cbfe2e3e9d5f949a814e8d5d664db771a31
yannic <Yannic.wlns@gmail.com>
2019-01-04 09:40:20 +0100

    add threshold parameter

--- main.m
+++ main.m
@@ -1,8 +1,11 @@
-%Game: int
+%game: int
 game = 3;
 
+%threshold: float <= 1 && >= 0
+threshold = 0.5;
+
 for i = 2 : length(dir(strcat('assets/original/', int2str(game), '_*.jpg')))
-    corners = threshold(game,i,0.5);
+    corners = threshold(game,i,threshold);
     geometric_transformation(game, i, corners);
     canny(game, i);
     hough(game, i); 
