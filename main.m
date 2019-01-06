a6b444cea28440d70a9c0b42a215a30a14895b3a
Tobias Batik <tobias.batik@gmail.com>
2019-01-06 20:00:02 +0100

    bibtex zeugs

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 %game: int
-game = 3;
+game = 4;
 
 %threshold: float <= 1 && >= 0
 custom_threshold = 0.5;
@@ -8,7 +8,7 @@ for i = 1 : length(dir(strcat('assets/original/', int2str(game), '_*.jpg')))
     corners = threshold(game,i,custom_threshold);
     geometric_transformation(game, i, corners);
     canny(game, i);
-    if (game == 3) 
+    if (game == 3 || game == 4) 
         centerPoints = hough(game, i, 15,25,35,0.3); %durchschnittlicher radius der Kreise im entzerrten Bild = 20px
     else 
         centerPoints = hough(game, i, 15,25,35,0.37); %durchschnittlicher radius der Kreise im entzerrten Bild = 20px
